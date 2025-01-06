3d8d2e5f3df2351c644a61c43a304165773b0ed7
Santhi Raju <santhiraju32@gmail.com>
2025-01-06 21:34:19 +0530

    url

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin:['*'],
+    origin:['https://spidey-ui.vercel.app/'],
     methods:['POST','PUT','GET','DELETE'],
     credentials:true
 }));
