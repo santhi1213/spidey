cbff111b73385e0659e80d07fd444f29bbe5a343
Santhi Raju <santhiraju32@gmail.com>
2025-01-09 20:06:32 +0530

    added frontend port

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3001, // Replace 3001 with your desired port
+  },
 })
