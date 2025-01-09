d9142e5acbbf9cbc50fbfa2aff497853e064cd01
Santhi Raju <santhiraju32@gmail.com>
2025-01-09 21:28:38 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(cors({
     origin: 'https://spidey-frontend-glmm.onrender.com/', // Replace with your frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
