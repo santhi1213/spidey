51bb9e8f2c03e6eaf382f5d368c87cf608f916d7
Santhi Raju <santhiraju32@gmail.com>
2025-02-13 20:38:00 +0530

    vite error

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: process.env.PORT || 3000, // Use the PORT provided by Render or default to 3000
-    // host: true, // Allows external access
+    port: process.env.PORT || 3000,
+    allowedHosts: ['spidey-frontend-glmm.onrender.com'], // Add this line
   },
 })
